65b67b43f04ce49091f46e030a1364d0b4240b14
Daniel Jesus Rodriguez Londoño <daniel.rodriguez@tucorreo.com>
2025-11-26 15:07:23 -0500

    se agregan rutas al frontend

--- controllers/proyecto.js
+++ controllers/proyecto.js
@@ -1,12 +1,22 @@
 const proyecto = {
   home: (req, res) => {
     try {
-      res.render('login', { nombrePagina: 'Iniciar Sesión - ParkingPlus' });
+      res.render('login', { nombrePagina: 'login - ParkingPlus' });
     } catch (error) {
       console.error('Error al renderizar login:', error);
       res.status(500).send('Error al cargar la página');
     }
   },
+
+  login: (req, res) => {
+    try {
+      res.render('login', { nombrePagina: 'login - ParkingPlus' });
+    } catch (error) {
+      console.error('Error al renderizar login:', error);
+      res.status(500).send('Error al cargar la página');
+    }
+  },
+  
   contactenos: (req, res) => {
     try {
       res.render('nosotros', { nombrePagina: 'Nosotros - ParkingPlus' });
@@ -14,7 +24,25 @@ const proyecto = {
       console.error('Error al renderizar nosotros:', error);
       res.status(500).send('Error al cargar la página');
     }
+  },
+    menu: (req, res) => {
+    try {
+      res.render('menuprincipal', { nombrePagina: 'menu - ParkingPlus' });
+    } catch (error) {
+      console.error('Error al renderizar nosotros:', error);
+      res.status(500).send('Error al cargar la página');
+    }
+  },
+
+    parqueadero: (req, res) => {
+    try {
+      res.render('parqueaderos', { nombrePagina: 'parqueaderos - ParkingPlus' });
+    } catch (error) {
+      console.error('Error al renderizar nosotros:', error);
+      res.status(500).send('Error al cargar la página');
+    }
   }
+
 };
 
 export default proyecto;
--- routes/home.js
+++ routes/home.js
@@ -5,6 +5,9 @@ import express from 'express';
 const route = express.Router();
 
 route.get('/', proyecto.home);
+route.get('/login', proyecto.login);
 route.get('/nosotros', proyecto.contactenos);
+route.get('/parqueaderos', proyecto.parqueadero);
+route.get('/menu', proyecto.menu);
 
 export default route;
